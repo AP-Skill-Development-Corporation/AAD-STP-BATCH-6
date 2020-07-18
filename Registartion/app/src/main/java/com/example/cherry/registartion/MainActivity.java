492493036185faa87fcc5c38aeda38a46d64f386
chaitanya <chaitanyakarri963@gmail.com>
2020-07-18 15:11:52 +0530

    18-07-2020

--- Registartion/app/src/main/java/com/example/cherry/registartion/MainActivity.java
+++ Registartion/app/src/main/java/com/example/cherry/registartion/MainActivity.java
@@ -4,15 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et,et1,et2;
+    Spinner sp;
+    RadioGroup rg;
+    RadioButton rb;
+    CheckBox cb1,cb2,cb3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = findViewById(R.id.name);
+        et1 = findViewById(R.id.email);
+        et2 = findViewById(R.id.number);
+        sp = findViewById(R.id.branch);
+        rg = findViewById(R.id.rg);
+        cb1 = findViewById(R.id.tel);
+        cb2 = findViewById(R.id.hin);
+        cb3 = findViewById(R.id.eng);
     }
 
     public void register(View view) {
+        String name = et.getText().toString();
+        String email = et1.getText().toString();
+        String number = et2.getText().toString();
+        String branch = sp.getSelectedItem().toString();
+        int i = rg.getCheckedRadioButtonId();
+        rb = findViewById(i);
+        StringBuilder sb = new StringBuilder();
+        if (cb1.isChecked()){
+            sb.append("Telugu"+"\n");
+        }
+        if (cb2.isChecked()){
+            sb.append("Hindi"+"\n");
+        }
+        if (cb3.isChecked()){
+            sb.append("English");
+        }
+        String lang = sb.toString();
+        Toast.makeText(this, ""+name+"\n"+email+"\n"+number
+                +"\n"+branch+"\n"+rb.getText()+"\n"+lang, Toast.LENGTH_SHORT).show();
     }
 }
