34df901db4d941094b896fae506a011f1cbbb33e
chaitanya <chaitanyakarri963@gmail.com>
2020-07-18 15:33:42 +0530

    updated

---
+++ AlertExample/app/src/main/java/com/example/cherry/alertexample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.cherry.alertexample;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alert(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Android Workshop");
+        builder.setMessage("Are you sure want to exit ?");
+        builder.setIcon(R.drawable.ic_icon);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Alert Cancelled",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+
+    }
+}
