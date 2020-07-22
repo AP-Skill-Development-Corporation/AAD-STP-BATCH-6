133fe3ae5213c3688555ad55d0e06c0caeb146bd
chaitanya <chaitanyakarri963@gmail.com>
2020-07-22 15:43:30 +0530

    22-07-2020

--- ExampleWork/app/src/main/java/com/example/cherry/examplework/MainActivity.java
+++ ExampleWork/app/src/main/java/com/example/cherry/examplework/MainActivity.java
@@ -1,22 +1,39 @@
 package com.example.cherry.examplework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
-    OneTimeWorkRequest firstrequest;
+    OneTimeWorkRequest firstrequest,secondrequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        firstrequest = new OneTimeWorkRequest.Builder(FirstWork.class).build();
+        firstrequest = new OneTimeWorkRequest
+                .Builder(FirstWork.class)
+                .build();
+        Constraints constraints = new Constraints
+                .Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+        secondrequest = new OneTimeWorkRequest.
+                Builder(SecondWork.class)
+                .setInitialDelay(5, TimeUnit.SECONDS)
+                .setConstraints(constraints)
+                .build();
     }
 
     public void dowork(View view) {
-        WorkManager.getInstance(this).enqueue(firstrequest);
+        WorkManager.getInstance(this).beginWith(firstrequest).
+                then(secondrequest).enqueue();
+       // WorkManager.getInstance(this).enqueue(secondrequest);
     }
 }
--- ExampleWork/app/src/main/java/com/example/cherry/examplework/SecondWork.java
+++ ExampleWork/app/src/main/java/com/example/cherry/examplework/SecondWork.java
@@ -1,4 +1,22 @@
 package com.example.cherry.examplework;
 
-public class SecondWork {
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.WorkManager;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class SecondWork extends Worker {
+    public SecondWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.i("workey","This is my second work");
+        return Result.success();
+    }
 }
