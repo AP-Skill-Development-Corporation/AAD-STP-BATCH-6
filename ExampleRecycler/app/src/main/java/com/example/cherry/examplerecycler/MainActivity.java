4b3c84e1866c60a1bd85a231852ecc62bf7e6b32
chaitanya <chaitanyakarri963@gmail.com>
2020-07-24 15:48:58 +0530

    24-07-2020

---
+++ ExampleRecycler/app/src/main/java/com/example/cherry/examplerecycler/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cherry.examplerecycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv = findViewById(R.id.rv);
+    }
+}
