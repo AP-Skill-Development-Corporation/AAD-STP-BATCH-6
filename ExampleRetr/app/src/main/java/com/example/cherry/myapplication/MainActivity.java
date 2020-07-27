770c4e6a6af0b3356d9decd04a7b8983bc38cbee
chaitanya <chaitanyakarri963@gmail.com>
2020-07-27 14:08:04 +0530

    27-07-2020

---
+++ ExampleRetr/app/src/main/java/com/example/cherry/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cherry.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView rv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv = findViewById(R.id.rv);
+    }
+}
---
+++ ExampleRetr/app/src/main/java/com/example/cherry/myapplication/MyAdapter.java
@@ -0,0 +1,40 @@
+package com.example.cherry.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    Context ct;
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(ct).inflate(R.layout.row,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv;
+        TextView tv;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iv = itemView.findViewById(R.id.iv);
+            tv = itemView.findViewById(R.id.tv);
+        }
+    }
+}
